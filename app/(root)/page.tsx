b22ff109c5f0c9fbdb5d64cfaa252dd008382ca9
not-meet <mjmeet.279@gmail.com>
2025-03-22 17:40:36 +0530

    fixing auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
+import { currentUser } from "@clerk/nextjs/server";
 import React from "react"
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect('/sign-in');
+
   return (
     <>
       <h1>hey this is the home page</h1>
